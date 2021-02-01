112285feb49ce636ed581e46d0bf874093aad082
hussnainjav3d <55350630+hussnainjav3d@users.noreply.github.com>
2021-02-02 01:09:40 +0500

    Adding data in data.js file and delting iomages

--- src/global/data.js
+++ src/global/data.js
@@ -20,9 +20,39 @@ const data = {
     img: "https://i.ibb.co/mzqWJvF/new2.png",
   },
   "nike-sply-360": {
-    name: "Nike SPLY",
+    name: "Nike SPLY Black",
     price: "$144.99",
-    img: "https://i.ibb.co/mzqWJvF/new2.png",
+    img: "https://i.ibb.co/TTw7qmg/new3.png",
+  },
+  "nike-sply-370": {
+    name: "Nike SPLY Gray",
+    price: "$144.99",
+    img: "https://i.ibb.co/NrMZMPN/new4.png",
+  },
+  "nike-sply-380": {
+    name: "Nike SPLY White",
+    price: "$144.99",
+    img: "https://i.ibb.co/xXwZQNp/new5.png",
+  },
+  "nike-free-tr": {
+    name: "Nike Free TR",
+    price: "$124.99",
+    img: "https://i.ibb.co/4NP39g8/women1.png",
+  },
+  "nike-free-tr-orange": {
+    name: "Nike Free TR",
+    price: "$124.99",
+    img: "https://i.ibb.co/GMKDX57/women2.png",
+  },
+  "nike-gs-pink": {
+    name: "Nike GS Pink",
+    price: "$124.99",
+    img: "https://i.ibb.co/qJ23w7p/women3.png",
+  },
+  "nike-get-5": {
+    name: "Nike Get 5",
+    price: "$124.99",
+    img: "https://i.ibb.co/kx8CxWX/women4.png",
   },
 };
 
